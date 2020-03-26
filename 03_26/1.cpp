a0578fc16fc2859af1651b1fae88e15b43b29a2e
pozdnyako <pozdnyak2001@mail.ru>
2020-03-26 10:00:35 +0300

    1st prog

---
+++ 03_26/1.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+#include <algorithm>
+
+int main() {
+    for(int n = 1; n < 100; n ++) {
+        std::cout.width(10);
+        std::cout.fill(' ');
+        std::cout.precision(5);
+        std::cout.setf(std::ios::right);
+        std::cout << n;
+        std::cout.width(10);
+        std::cout.fill(' ');
+        std::cout.precision(5);
+        std::cout.setf(std::ios::right);
+        std::cout << log(n);
+        std::cout.width(10);
+        std::cout.fill(' ');
+        std::cout.precision(5);
+        std::cout.setf(std::ios::right);
+        std::cout << log10(n);
+        std::cout << "\n";
+    }
+}
